a73e723bf8dc55c4bee5b2ca07b88a2ab296c1eb
elman-asker <81957474+elman-asker@users.noreply.github.com>
2021-09-01 09:19:16 +0400

    Add files via upload

---
+++ Azerbaijan xeritesi/Azerbaijan xeritesi/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Azerbaijan_xeritesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("BAKI");
+            comboBox1.Items.Add("NAXCIVAN");
+            comboBox1.Items.Add("XACMAZ");
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if(comboBox1.Text == "BAKI")
+            {
+                label2.Visible=true;
+                label3.Visible = false;
+                label4.Visible = false;
+
+
+            }
+            else if (comboBox1.Text == "NAXCIVAN" )
+            {
+                label2.Visible = false;
+                label3.Visible = true;
+                label4.Visible = false;
+                
+            }
+            else if (comboBox1.Text == "XACMAZ")
+            {
+                label2.Visible = false;
+                label3.Visible = false;
+                label4.Visible = true;
+
+            }
+
+
+
+        }
+   
+        
+    }
+}
